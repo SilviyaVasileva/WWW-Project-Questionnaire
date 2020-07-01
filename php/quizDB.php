10a3a93ad3fa88b3536fa3932b705dd05e14c5c1
Veniyana <veniyana.ts@gmail.com>
2020-07-01 10:26:40 +0300

    Moved files to folders

---
+++ php/quizDB.php
@@ -0,0 +1,15 @@
+
+<?php
+$host = "localhost";
+$dbname = "quiz";
+$user = "azaz";
+$pass = "parola123";
+
+// $host = "localhost";
+// $dbname = "quiz";
+// $user = "root";
+// $pass = "";
+
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+// $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXEPTION);
+
