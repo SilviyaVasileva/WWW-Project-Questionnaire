7b3fa274da5b4959e2c304b48b4943427688962b
SilviyaVasileva <60818785+SilviyaVasileva@users.noreply.github.com>
2020-07-04 14:48:03 +0300

    diferent menu view for student and lector

--- php/addQuiz.php
+++ php/addQuiz.php
@@ -16,67 +16,69 @@ require_once('quizDB.php');
 			echo '<a href="../php/logout.php?logout">Изход</a><br/>';
 			echo '<a href="../php/menu.php?menu">Меню</a>';
 
-			$sql_tests = "SELECT * FROM `test` WHERE type like '%quiz%' AND creator_id = ".$_SESSION['id'];
-		 	$result_tests = $conn->query($sql_tests) or die("failed!");
+			if ($_SESSION['type'] == 'lector') {
 
-			if (isset($_POST['createQuiz'])) {
-			 	$testName = $_POST['testName'];
-		 		$sql = "INSERT INTO test (test_name, creator_id, type) VALUES (?,?,?)";
-				$stmtinsert = $conn->prepare($sql);
-				$result = $stmtinsert->execute([$testName, $_SESSION['id'], 'quiz']);
-				if($result) {
-					echo "created! <br />";
+				$sql_tests = "SELECT * FROM `test` WHERE type like '%quiz%' AND creator_id = ".$_SESSION['id'];
+			 	$result_tests = $conn->query($sql_tests) or die("failed!");
+
+				if (isset($_POST['createQuiz'])) {
+				 	$testName = $_POST['testName'];
+			 		$sql = "INSERT INTO test (test_name, creator_id, type) VALUES (?,?,?)";
+					$stmtinsert = $conn->prepare($sql);
+					$result = $stmtinsert->execute([$testName, $_SESSION['id'], 'quiz']);
+					if($result) {
+						header("location:addQuiz.php");
+					}
+					else {
+						echo "Wrong input! <br />";
+					}
 				}
-				else {
-					echo "Wrong input! <br />";
+				if (isset($_POST['addQuestion'])) {
+			 		// Create a question
+			 		$test_id = $_POST['test_names'];
+				 	$question = $_POST['question'];
+
+				 	echo $test_id."  въпрос ".$question;
+
+					 	$sql_create_q = "INSERT INTO `question` (test_id, description, points, correct_answer_id) VALUES (?,?,?,?)";
+						$stmtinsert_question = $conn->prepare($sql_create_q);
+						$result_create_q = $stmtinsert_question->execute([$test_id, $question, 'NULL', 'NULL']);
+
+
+				 	// Get question id
+
+				 	$sql_qid = "SELECT * FROM `question` WHERE description = '".$question."'";
+			 		$result_q = $conn->query($sql_qid) or die("failed!");
+
+			 		$q_id = 0;
+
+			 		while ($row_q = $result_q->fetch(PDO::FETCH_ASSOC)) {
+			 			$q_id = $row_q['id'];
+			 		}
+
+			 		// create answers
+
+			 		$answ1 = $_POST['answ1'];
+			 		$answ2 = $_POST['answ2'];
+			 		$answ3 = $_POST['answ3'];
+			 		$answ4 = $_POST['answ4'];
+
+					$sql_create_answ1 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
+					$stmtinsert_answ1 = $conn->prepare($sql_create_answ1);
+					$result_answ1 = $stmtinsert_answ1->execute([$q_id, 1, $answ1]);
+
+					$sql_create_answ2 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
+					$stmtinsert_answ2 = $conn->prepare($sql_create_answ2);
+					$result_answ2 = $stmtinsert_answ2->execute([$q_id, 2, $answ2]);
+
+					$sql_create_answ3 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
+					$stmtinsert_answ3 = $conn->prepare($sql_create_answ3);
+					$result_answ3 = $stmtinsert_answ3->execute([$q_id, 3, $answ3]);
+
+					$sql_create_answ4 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
+					$stmtinsert_answ4 = $conn->prepare($sql_create_answ4);
+					$result_answ4 = $stmtinsert_answ4->execute([$q_id, 4, $answ4]);
 				}
-			}
-			if (isset($_POST['addQuestion'])) {
-		 		// Create a question
-		 		$test_id = $_POST['test_names'];
-			 	$question = $_POST['question'];
-
-			 	echo $test_id."  въпрос ".$question;
-
-				 	$sql_create_q = "INSERT INTO `question` (test_id, description, points, correct_answer_id) VALUES (?,?,?,?)";
-					$stmtinsert_question = $conn->prepare($sql_create_q);
-					$result_create_q = $stmtinsert_question->execute([$test_id, $question, 'NULL', 'NULL']);
-
-
-			 	// Get question id
-
-			 	$sql_qid = "SELECT * FROM `question` WHERE description = '".$question."'";
-		 		$result_q = $conn->query($sql_qid) or die("failed!");
-
-		 		$q_id = 0;
-
-		 		while ($row_q = $result_q->fetch(PDO::FETCH_ASSOC)) {
-		 			$q_id = $row_q['id'];
-		 		}
-
-		 		// create answers
-
-		 		$answ1 = $_POST['answ1'];
-		 		$answ2 = $_POST['answ2'];
-		 		$answ3 = $_POST['answ3'];
-		 		$answ4 = $_POST['answ4'];
-
-				$sql_create_answ1 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
-				$stmtinsert_answ1 = $conn->prepare($sql_create_answ1);
-				$result_answ1 = $stmtinsert_answ1->execute([$q_id, 1, $answ1]);
-
-				$sql_create_answ2 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
-				$stmtinsert_answ2 = $conn->prepare($sql_create_answ2);
-				$result_answ2 = $stmtinsert_answ2->execute([$q_id, 2, $answ2]);
-
-				$sql_create_answ3 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
-				$stmtinsert_answ3 = $conn->prepare($sql_create_answ3);
-				$result_answ3 = $stmtinsert_answ3->execute([$q_id, 3, $answ3]);
-
-				$sql_create_answ4 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
-				$stmtinsert_answ4 = $conn->prepare($sql_create_answ4);
-				$result_answ4 = $stmtinsert_answ4->execute([$q_id, 4, $answ4]);
-
 			}
 		}
 
--- php/addTest.php
+++ php/addTest.php
@@ -12,95 +12,84 @@ require_once('quizDB.php');
 <div class="phpTest">
 	<?php
 		session_start();
-		// echo $_SESSION['id'];
+		// check if the user is logged in
 		if(isset($_SESSION['user'])) {
 			echo '<a href="../php/logout.php?logout">Изход</a><br/>';
 			echo '<a href="../php/menu.php?menu">Меню</a>';
+			if ($_SESSION['type'] == 'lector') {
+				// if the user is logged and its lector in shows the form
 
-			$sql_tests = "SELECT * FROM `test` WHERE type = 'test' AND creator_id = ".$_SESSION['id'];
-		 	$result_tests = $conn->query($sql_tests) or die("failed!");
 
-			if (isset($_POST['createTest'])) {
-		 		// echo "user id created";
-			 	$testName = $_POST['testName'];
-		 		$sql = "INSERT INTO test (test_name, creator_id) VALUES (?,?)";
-				$stmtinsert = $conn->prepare($sql);
-				$result = $stmtinsert->execute([$testName, $_SESSION['id']]);
-				if($result) {
-					echo "created! <br />";
+				$sql_tests = "SELECT * FROM `test` WHERE type = 'test' AND creator_id = ".$_SESSION['id'];
+			 	$result_tests = $conn->query($sql_tests) or die("failed!");
+
+				if (isset($_POST['createTest'])) {
+			 		// create a test in db
+				 	$testName = $_POST['testName'];
+			 		$sql = "INSERT INTO test (test_name, creator_id) VALUES (?,?)";
+					$stmtinsert = $conn->prepare($sql);
+					$result = $stmtinsert->execute([$testName, $_SESSION['id']]);
+					if($result) {
+			            header("location:addTest.php");	
+					}
+					else {
+						echo "Wrong input! <br />";
+					}
 				}
-				else {
-					echo "Wrong input! <br />";
+				if (isset($_POST['createQuestion'])) {
+
+
+			 		// Create a question
+			 		$test_id = $_POST['test_names'];
+			 		// echo $test_id;
+				 	$question = $_POST['question'];
+				 	$points = $_POST['points'];
+				 	$correct_answer = $_POST['correct_answer'];
+
+				 	echo $test_id."  въпрос ".$question."  точки ".$points."  отговор ".$correct_answer;
+
+					 	$sql_create_q = "INSERT INTO `question` (test_id, description, points, correct_answer_id) VALUES (?,?,?,?)";
+						$stmtinsert_question = $conn->prepare($sql_create_q);
+						$result_create_q = $stmtinsert_question->execute([$test_id, $question, $points, $correct_answer]);
+
+
+				 	// Get question id
+
+				 	$sql_qid = "SELECT * FROM `question` WHERE description = '".$question."'";
+			 		$result_q = $conn->query($sql_qid) or die("failed!");
+
+			 		$q_id = 0;
+
+			 		while ($row_q = $result_q->fetch(PDO::FETCH_ASSOC)) {
+			 			$q_id = $row_q['id'];
+			 		}
+
+			 		// create answers
+
+			 		$answ1 = $_POST['answ1'];
+			 		$answ2 = $_POST['answ2'];
+			 		$answ3 = $_POST['answ3'];
+			 		$answ4 = $_POST['answ4'];
+
+					$sql_create_answ1 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
+					$stmtinsert_answ1 = $conn->prepare($sql_create_answ1);
+					$result_answ1 = $stmtinsert_answ1->execute([$q_id, 1, $answ1]);
+
+					$sql_create_answ2 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
+					$stmtinsert_answ2 = $conn->prepare($sql_create_answ2);
+					$result_answ2 = $stmtinsert_answ2->execute([$q_id, 2, $answ2]);
+
+					$sql_create_answ3 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
+					$stmtinsert_answ3 = $conn->prepare($sql_create_answ3);
+					$result_answ3 = $stmtinsert_answ3->execute([$q_id, 3, $answ3]);
+
+					$sql_create_answ4 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
+					$stmtinsert_answ4 = $conn->prepare($sql_create_answ4);
+					$result_answ4 = $stmtinsert_answ4->execute([$q_id, 4, $answ4]);
+
 				}
-			}
-			if (isset($_POST['createQuestion'])) {
-		 		// echo "<br/>user id created ";
-
-
-		 		// Create a question
-		 		$test_id = $_POST['test_names'];
-		 		// echo $test_id;
-			 	$question = $_POST['question'];
-			 	$points = $_POST['points'];
-			 	$correct_answer = $_POST['correct_answer'];
-
-			 	echo $test_id."  въпрос ".$question."  точки ".$points."  отговор ".$correct_answer;
-
-				 	$sql_create_q = "INSERT INTO `question` (test_id, description, points, correct_answer_id) VALUES (?,?,?,?)";
-					$stmtinsert_question = $conn->prepare($sql_create_q);
-					$result_create_q = $stmtinsert_question->execute([$test_id, $question, $points, $correct_answer]);
-
-
-			 	// Get question id
-
-			 	$sql_qid = "SELECT * FROM `question` WHERE description = '".$question."'";
-		 		$result_q = $conn->query($sql_qid) or die("failed!");
-
-		 		$q_id = 0;
-
-		 		while ($row_q = $result_q->fetch(PDO::FETCH_ASSOC)) {
-		 			$q_id = $row_q['id'];
-		 		}
-
-		 		// create answers
-
-		 		$answ1 = $_POST['answ1'];
-		 		$answ2 = $_POST['answ2'];
-		 		$answ3 = $_POST['answ3'];
-		 		$answ4 = $_POST['answ4'];
-
-				$sql_create_answ1 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
-				$stmtinsert_answ1 = $conn->prepare($sql_create_answ1);
-				$result_answ1 = $stmtinsert_answ1->execute([$q_id, 1, $answ1]);
-
-				$sql_create_answ2 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
-				$stmtinsert_answ2 = $conn->prepare($sql_create_answ2);
-				$result_answ2 = $stmtinsert_answ2->execute([$q_id, 2, $answ2]);
-
-				$sql_create_answ3 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
-				$stmtinsert_answ3 = $conn->prepare($sql_create_answ3);
-				$result_answ3 = $stmtinsert_answ3->execute([$q_id, 3, $answ3]);
-
-				$sql_create_answ4 = "INSERT INTO `answer` (question_id, answer_number, answer_text) VALUES (?,?,?)";
-				$stmtinsert_answ4 = $conn->prepare($sql_create_answ4);
-				$result_answ4 = $stmtinsert_answ4->execute([$q_id, 4, $answ4]);
-
-
-
-		 		// 	$sql = "INSERT INTO test (test_name, creator_id) VALUES (?,?)";
-				// $stmtinsert = $conn->prepare($sql);
-				// $result = $stmtinsert->execute([$testName, $_SESSION['id']]);
-				// if($result) {
-				// 	echo "Registered! <br />";
-				// }
-				// else {
-				// 	echo "Wrong input! <br />";
-				// }
 			}
 		}
-
-			
-		// 	echo $username."<br />";
 
 	?>
 </div>
